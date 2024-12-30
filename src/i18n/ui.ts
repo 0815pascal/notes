1f582eaee603b031ac1a360ccc8872db0e6aad55
breadpitt <pascal.witzig@gmail.com>
2024-12-30 13:47:29 +0100

    add astro

---
+++ src/i18n/ui.ts
@@ -0,0 +1,40 @@
+/**
+ * This configures the translations for all ui text in your website. 
+ * 
+ * All languages will follow this ordering/structure and will fallback to the
+ * default language for any entries that haven't been translated 
+ */
+import type { SupportedLanguage } from "src/utils/i18n";
+
+export default {
+    "en": {
+        "site.title": {
+            text: "Astro Theme Cody"
+        },
+        "site.description": {
+            text: "A minimalist blog theme built with Astro. A quick and easy starter build for anyone who wants to start their own blog."
+        },
+        "profile.description": {
+            text: "your bio description"
+        },
+        "blog.lastUpdated": {
+            text: "Last updated:"
+        },
+        "sidebar.tableOfContents": {
+            text: "Table of Contents"
+        },
+        "project.platform": {
+            text: "PLATFORM"
+        },
+        "project.stack": {
+            text: "STACK"
+        },
+        "project.website": {
+            text: "WEBSITE"
+        }
+    },
+} as const satisfies TranslationUIEntries;
+
+type TranslationUIEntries = Record<SupportedLanguage, Record<string, UIEntry>>;
+
+export type UIEntry = { text: string };
